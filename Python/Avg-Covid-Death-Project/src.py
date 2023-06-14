0c454f66105823eff026f7fb5e383815ae765f29
Shaun Partridge <47838616+ShaunJPartridge@users.noreply.github.com>
2023-06-14 17:14:21 -0400

    Update src.py

--- Python/Avg-Covid-Death-Project/src.py
+++ Python/Avg-Covid-Death-Project/src.py
@@ -1,74 +1,42 @@
 ## Name: Shaun Partridge
 ## Course: CMPS-4553: Topics in Computational Science
 ## File: src.py
-## Date: 9/23/2021
+## Date: 9/9/2021
 ## Description: This program will open a csv file and extract the data from it to
 ## calculate and print the mean chance of someone dying from COVID-19 and the standard
 ## deviation of the mean.
 
 import csv
 import math
-import numpy as np
-from tabulate import tabulate
-#from beautifultable import BeautifulTable
-#from numpy.core.defchararray import isalpha, isnumeric
 
 
 if __name__ == '__main__':
 
-    # open file and read data
-    with open("State_to_State_Migrations_Table_2019.csv") as infile:
-        data = list(csv.reader(infile,delimiter=','))
-
-    # this row or list has all of the state column headers
-    state_col_headers = list(data[6]) 
+    #open file and read data
+    with open("CovidDeathCounts.csv") as infile:
+        data = list(csv.reader(infile))
+    #examine header fow to find column of interest
+    ageIndex = data[0].index("Age")
+    covidDeathsIndex = data[0].index("COVID-19 Deaths")
     
-    # get index of last column, which is 55
-    totIndex = state_col_headers.index("Wyoming") 
+    #access field of interest in records and calculate/display stats
+    ages = [int(row[ageIndex]) for row in data[1:] if row[ageIndex] != '']
+    c_deaths = [int(row[covidDeathsIndex]) for row in data[1:] if row[covidDeathsIndex] != '']
 
-    # each array or row in each sub-array is a column with a states' migration data
-    XtoY = np.array([row[5:totIndex+1] for row in data[11:]])
+    res = [float(int(row[ageIndex])* int(row[covidDeathsIndex])) for row in data[1:] if row[ageIndex] != '' and row[covidDeathsIndex] != '']
     
-    # Assign the transpose of the array to the array with blank elements removed
-    XtoY = np.array([np.delete(row,-1) for row in XtoY.T[0:]])
+    freqs = sum(res)
+    N = sum(c_deaths)
+    mean = float(freqs/N)
+    std = 0
 
-    # cleaning up data here
-    where_are_na = XtoY == 'N/A'
-    XtoY[where_are_na] = 0
-
-    # create the array for the final time and change the dtype to int
-    XtoY = np.array([[el.replace(',','') for el in row] for row in XtoY],dtype=int)
-
-    # create dictionary with state column headers as keys and each numpy array in XtoY as values
-    simDict = dict(zip(state_col_headers[5:],XtoY))
-
-    # create another dictionary to hold tuples of similar states as keys and the  
-    # similarity scores between the two states as values
-    myDict = {}
-
-    # loop through simDicts' items
-    for k,v in simDict.items():
-        # loop through numpy array of XtoY values
-        for i in range(len(v)):
-            # if the XtoY value is bigger than the sum of the XtoY values divided by 50
-            if v[i] > int(np.sum(v)/50):
-                # Assign to a key, which is a tuple consisting of the current state and the state that's similar 
-                # to it, the XtoY value
-                myDict[(k,list(simDict.keys())[i])] = v[i]
-     
-    # get the 10 most similar pairs of states, by sorting dictionary 
-    most_similar_states = sorted(myDict,key=myDict.get,reverse=True)[:10]
+    for i in range(len(ages)):
+        std += (ages[i]-mean)**2
+    std = math.sqrt(std/(N-1))
     
-    # create table with tuples consisting of numbers 1-10 and tuples of paired similar states
-    table = zip([i for i in range(1,11)],most_similar_states)
+    with open('outfile.txt','w') as of:
+        of.write("The average chance of someone dying from COVID-19 is: {}.\n".format(mean))
+        of.write("The standard deviation of the mean is: {}.".format(std))
 
-    # create a temp table to print to screen, since tabulate uses the table for the output file
-    tmp_table = zip([i for i in range(1,11)],most_similar_states)
-
-    # print table using module, tabulate
-    print("The table below shows the 10 most similar pairs of states:\n\n",tabulate(tmp_table,headers=["Rank","Similar States"]))
-
-    # write the output and table into a file
-    with open("output.txt",'w') as of:
-        of.write("The table below shows the 10 most similar pairs of states:\n\n")
-        of.write(tabulate(table,headers=["Rank","Similar States"],tablefmt="grid"))
+    print("The average chance of someone dying from COVID-19 is: {}.".format(mean))
+    print("The standard deviation of the mean is: {}.".format(std))
