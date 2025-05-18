a5de2a86a0f07b1c0b19fdfacd755cbdf2953b03
Shaun Partridge <spartridgeb15@gmail.com>
2025-05-18 17:09:49 -0400

    update

--- Python/Customer-Churn-Prediction-Project/main.py
+++ Python/Customer-Churn-Prediction-Project/main.py
@@ -6,71 +6,87 @@ import kaggle
 import os
 import pandas as pd
 
-# Get credentials for GCP
+# Initialize credentials path and object to access GCP resources
 credentials_path = 'C:/Users/spart/GCP/customer-churn-project-serv-accnt-creds/amazon-deliveries-project-c2fedb2a708b.json'
-# Create a credentials object
 credentials = service_account.Credentials.from_service_account_file(credentials_path)
 
-# Authenticate with Kaggle API
-kaggle.api.authenticate()
-
-# Initialize variables to store dataset, GCP project ID, and dataset ID
+# Initialize variables to store dataset path and name, GCP project ID, dataset ID, 
+# bucket, external and final table names 
 dataset = 'mustafakeser4/bigquery-churn-dataset'
 file_name = 'churn.csv'
 local_path = f'./{file_name}'
 project_id = 'amazon-deliveries-project'
 dataset_id = 'customer_churn_data'
-table_id = 'external_churn_table'
-
-# Download the dataset
-kaggle.api.dataset_download_files(dataset, path='.', unzip=True)
-
-# Download the dataset metadata
-kaggle.api.dataset_metadata(dataset, path='.')
-
-# Read in .csv file into dataframe
-df = pd.read_csv(local_path)
-
-# Initialize variables for GCS bucket and blob name
 bucket_name = 'customer_churn_project_bucket'
-blob_name = 'churn.csv'
-df.to_csv(blob_name, index=False)
-
-# Initialize Storage client to access GCP storage bucket
-client = storage.Client(credentials=credentials, project=project_id)
-bucket = client.bucket(bucket_name)
-blob = bucket.blob(blob_name)
-blob.upload_from_filename(local_path)
+external_table = ''
+final_table = f'{dataset_id}.churn'
 
 
-# Function to create a BigQuery view so the external table can be connected to in
-# Power BI
-def create_view(table_id):
-
-    # Initialize BigQuery client
-    Client = bigquery.Client(credentials=credentials, project=project_id)
-
-    # Define the view name and the SQL query to create it
-    view_id = 'vw_churn_table'
-    sql_query = f"""
-        CREATE OR REPLACE VIEW `{project_id}.{dataset_id}.{view_id}` AS
-        SELECT * FROM `{project_id}.{dataset_id}.{table_id}`
+def get_data():
+    """
+    Function to download the dataset from Kaggle and upload it to Google Cloud Storage (GCS).
+    This function uses the Kaggle API to download the dataset and then uploads it to a specified GCS bucket.
     """
 
-    # Create the view
-    Client.query(sql_query).result()  # Wait for the query to finish
+    # Authenticate with Kaggle API
+    kaggle.api.authenticate()
 
-    print(f'Created view {view_id}')
+    # Download the dataset
+    kaggle.api.dataset_download_files(dataset, path='.', unzip=True)
 
-# Function to create/update a clean native BigQuery table to be connected to in Power BI,
-# using the external table, upon being triggered by the dataset being updated in the GCS bucket.
-def gcs_triggered_cleaning(event, context):
+    # Download the dataset metadata
+    kaggle.api.dataset_metadata(dataset, path='.')
 
-    Client = bigquery.Client(credentials=credentials, project=project_id)
+    # Read in .csv file into dataframe
+    df = pd.read_csv(local_path)
 
-    # BigQuery SQL to clean and copy the data
-    query = """
-        CREATE OR REPLACE TABLE `{project_id}.{dataset_id}.churn` AS
+    # Set values for GCS bucket and blob name
+    blob_name = file_name
+    df.to_csv(blob_name, index=False)
+
+    # Initialize Storage client to access GCP storage bucket
+    client = storage.Client(credentials=credentials, project=project_id)
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(local_path)
+
+    print(f'{bucket_name} was successfully updated.')
+
+
+def create_final_table():
+    """
+    Function to create external configurations and table, and final table for BigQuery.
+    This function creates an external table in BigQuery that points to a CSV file in Google Cloud Storage (GCS).
+    The CSV file is expected to have a header row, and the function will skip this row when reading the data.
+    The external table is then used to transform the data and create a final native table in BigQuery.
+    The final table is then connected to in Power BI for analysis.
+    """
+
+    # Initialize BigQuery client
+    client = bigquery.Client(credentials=credentials, project=project_id)
+
+    # Define names for external table name & config variables
+    external_table_id = f'{project_id}.{dataset_id}.external_churn_table'
+    external_source_format = 'CSV'
+    uri = [f'gs://{bucket_name}/{file_name}']
+
+    # Define ExternalConfig object with external configurations
+    external_job_config = bigquery.ExternalConfig(external_source_format)
+    external_job_config.source_uris = uri # Specify the GCS URI of the source file
+    external_job_config.options.skip_leading_rows = 1  # Skip the header row
+    external_job_config.options.field_delimiter = ','  # Specify the delimiter
+    external_job_config.autodetect = True  # Let BigQuery detect the schema
+
+    # Create the external table
+    external_table = bigquery.Table(external_table_id)
+    external_table.external_data_configuration = external_job_config
+    client.create_table(external_table, exists_ok=True)
+    
+    print(f'External table {external_table_id} created.')
+
+    # Clean and transform into the final table
+    query = f"""
+        CREATE OR REPLACE TABLE `{final_table}` AS
         SELECT
             TRIM(LOWER(customer_id)) AS customer_id,
             state,
@@ -91,58 +107,17 @@ def gcs_triggered_cleaning(event, context):
             SAFE_CAST(total_intl_charge AS FLOAT64) AS total_intl_charge,
             SAFE_CAST(number_customer_service_calls AS INT64) AS number_customer_service_calls,            
             SAFE_CAST(churn AS INT64) AS churn
-        FROM `{project_id}.{dataset_id}.{table_id}`
+        FROM `{external_table}`
     """
 
-    query_job = Client.query(query)
-    query_job.result()
-
-    print("Cleaned table successfully updated.")
-
-
-# Function to load the data into BigQuery using an External table
-def create_table():
-
-    # Initialize BigQuery client
-    Client = bigquery.Client(credentials=credentials, project=project_id)
-
-    # Define table name, ref, and the GCS URI of the file
-    table_ref = f'{project_id}.{dataset_id}.{table_id}'
-    data_uri = f'gs://{bucket_name}/{blob_name}'
-
-    # Configure the external data source
-    external_config = bigquery.ExternalConfig('CSV')
-    external_config.source_uris = [data_uri]
-    external_config.options.skip_leading_rows = 1  # Skip the header row
-    external_config.options.field_delimiter = ','  # Specify the delimiter
-    external_config.autodetect = True # Let BiqQuery detect the schema
-
-    # Create the external table
-    table = bigquery.Table(table_ref)
-    table.external_data_configuration = external_config
-    
-    try:
-        # Try to fetch existing table
-        existing_table = Client.get_table(table_ref)
-
-        # Update the external configuration
-        existing_table.external_data_configuration = external_config
-        
-        # Update the table
-        updated_table = Client.update_table(existing_table, ['external_data_configuration'])
-        print(f'Updated existing table: {updated_table.table_id}')
-    except Exception as not_found:
-        # If the table does not exist, create a new one
-        created_table = Client.create_table(table)
-        print(f'Created new table: {created_table.table_id}')
-
-    # Clean the table
-    gcs_triggered_cleaning()
-
-    # Create the view so the data can be accessed in Power BI
-    #create_view(table_id)
+    client.query(query).result()
+    print(f'Cleaned data loaded into {final_table}')
 
 
 if __name__ == "__main__":
-    # Create the table and view
-    create_table()
+
+    # Scrape the data from Kaggle and upload it to GCS
+    get_data()
+
+    # Create the external and final tables in BigQuery 
+    create_final_table()
